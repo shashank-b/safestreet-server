ec02b90768c7a26737dc8b27d16617ee9c2c800d
Rahul Bishain <rahul.bishain@gmail.com>
2016-06-09 07:44:36 +0530

    Partial distance, warning fix, Speed
    Partial Distance: new field is added in autometed pothole model to
    account distance while ride is On
    warning fix: sklearn reshape warning fixed
    speed: avg speed of ride is not stored
    Linear model used for classification
    Bug fix: feature calculations done on smoothen data

--- auto_pothole/models.py
+++ auto_pothole/models.py
@@ -10,8 +10,9 @@ class AutomatedPotholes(models.Model):
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
     classifier_output = models.CharField(max_length=1, default='0')
-    pred_prob = models.FloatField(null=True)	
+    intensity_score = models.FloatField(null=True)
     detection_time = models.DateTimeField(null=True)
+    partial_distance = models.FloatField(null=True)
     
     def __str__(self):
         return "Id: %d, ReporterId: %d, Data: %c" % (self.id, self.reporter.id, self.classifier_output)
@@ -21,10 +22,11 @@ class RideDetails(models.Model):
     vehicle_type = models.CharField(max_length=1, default='D')
     start_time = models.DateTimeField(null=True)
     stop_time = models.DateTimeField(null=True)
-    speed = models.FloatField(null=True)	    
-    distance = models.FloatField(null=True)	
-    app_version = models.TextField()	    	
-    pothole_count = models.IntegerField(null=True)	    	
+    speed = models.FloatField(null=True)
+    distance = models.FloatField(null=True)
+    app_version = models.TextField()
+    pothole_count = models.IntegerField(null=True)
+    phone_model = models.TextField(null=True)
      
     def __str__(self):
         return "Id: %d, ReporterId: %d" % (self.id, self.reporter.id)
--- auto_pothole/views.py
+++ auto_pothole/views.py
@@ -12,57 +12,53 @@ from .serializers import *
 from django.core.files.base import ContentFile
 from django.shortcuts import render
 import datetime
-
+import numpy as np
+import os
+from serverPothole import settings
+from sklearn import preprocessing
 
 ############################################################
 import sys,math
 from sklearn.externals import joblib
 
-def getReorientedData(data, angleX, angleY):
-    cosY = math.cos(yAngle);
-    sinY = math.sin(yAngle);
-    cosX = math.cos(xAngle);
-    sinX = math.sin(xAngle);
-    
-    reorientedData = []	    
-    for i in range(len(data)):
-        X = data[i][0];
-        Y = data[i][1];
-        Z = data[i][2];
-       
-        tmp = -X*sinY + Z*cosY;
-        newx = X*cosY + Z*sinY;
-        newz = Y*cosX - tmp*sinX;
-        newy = Y*sinX + tmp*cosX;
-        reorientedData.append([newx,newy,newz])
-    return reorientedData
-    	
-
 def getSmoothData(data,winsize):
     smoothSize  = winsize / 2
-    smoothData = []    
+    smoothData = []
     d = []
 
     for i in range(smoothSize):
         d.append([0,9.8, data[0][2], data[0][3], data[0][4], data[0][5]])
-    
-    data =     d + data
+
+    data = d + data
     window = []
 
-    for i in range(len(data)):        
+    for i in range(len(data)):
         xy = data[i][0]
-        z = data[i][1]        
+        z = data[i][1]
         window.append([xy,z])
 
-        if(len(window) >= smoothSize):            
+        if(len(window) >= smoothSize):
             Z = map(lambda p:p[1], window)
             XY = map(lambda p:p[0], window)
             smoothData.append([sum(XY)/len(XY), sum(Z)/len(Z), data[i][2], data[i][3], data[i][4],data[i][5]])            
             window = window[1:]
-    return smoothData
+
+    starttime = 0
+    for i in range(len(smoothData)):
+        if(smoothData[i][4] != ''):
+            starttime = long(smoothData[i][4]) - 500
+
+    finalSmoothdata = []
+    for i in range(len(smoothData)):
+        if(smoothData[i][4] != ''):
+            finalSmoothdata.append([smoothData[i][0],smoothData[i][1],smoothData[i][2],smoothData[i][3],smoothData[i][4],smoothData[i][5]])
+        else:
+            finalSmoothdata.append([smoothData[i][0],smoothData[i][1],smoothData[i][2],smoothData[i][3],str(starttime),smoothData[i][5]])
+
+    return finalSmoothdata
 
 
-def getMaxMin(Z):        
+def getMaxMin(Z):
     return max(Z) - min(Z)
 
 def getVariance(Z):
@@ -71,40 +67,38 @@ def getVariance(Z):
     varz = sum(map(lambda p:(p-meanz)*(p-meanz), Z))/len(Z)
     return varz
 
-def getFeatures(data):        
+def getFeatures(data):
     Z = map(lambda p:p[1], data)
     return [getMaxMin(Z),getVariance(Z)]
 
 def cal_features(data,winsize):
-    smoothData = getSmoothData(data,winsize)    
     features = []
     
     startIndex = 0
     half_sec_index = ""
 
-
     for i in range(len(data)):
         if (data[startIndex][4] != data[i][4]):
-            half_sec_index = i            
+            half_sec_index = i
             break
 
     finaldata = []
-    for i in range(len(data)):    
+    for i in range(len(data)):
         if ( data[half_sec_index][4] != data[i][4] and data[i][4] != data[startIndex][4]):
-            feature = getFeatures( data[startIndex:i] )            
+            feature = getFeatures( data[startIndex:i] )
             finaldata.append([feature[0], feature[1], data[startIndex][2], data[startIndex][3], data[i][4]])
             
             startIndex = half_sec_index
             half_sec_index = i
-    
+
     feature = getFeatures( data[startIndex:i] )
     finaldata.append([feature[0], feature[1], data[startIndex][2], data[startIndex][3], data[startIndex][4]])
-    
+
     return finaldata
 
 
 
-def getData(line,winsize):
+def getParsedData(line,winsize):
     line = line[1:len(line)-2]
     values = line.split(";")
 
@@ -113,30 +107,29 @@ def getData(line,winsize):
     lon = ""
     time = ""
     speed = ""
-    
+
     for i in range(len(values)):
-    	
-        if( values[i][1:2].isalpha() ):	    
-            key_val = values[i].split(":")            
+        if( values[i][1:2].isalpha() ):
+            key_val = values[i].split(":")
             key = key_val[0]
             val = key_val[1]
 
-            if(key == "Lat"):                
+            if(key == "Lat"):
                 lat = val
 
             if(key == "Lon"):                
                 lon = val
 
-            if(key == "Speed" or key == " Speed"):                
+            if(key == "Speed" or key == " Speed"):
                 speed = val
 
-            if(key == "TIME"):                                
+            if(key == "TIME"):
                 time = val
         else:
-            val = values[i].split(",")                        
+            val = values[i].split(",")
             data.append([float(val[0]),float(val[1]), lat, lon, time, speed])
 
-    return cal_features(data,winsize)        
+    return data
 
 #############################################################
 class AddPothole(APIView):
@@ -158,10 +151,16 @@ class AddPothole(APIView):
         pothole_event_data = request.data.get('pothole_event_data')
         vehicle_type = request.data.get('vehicle_type')
         win_size = request.data.get('win_size')
+        partial_distance = request.data.get('partial_distance')
         reporters_time = request.data.get('CurrentTime')
-        server_time = int(datetime.datetime.now().strftime("%s")) * 1000 
-        offset = long(server_time) - long(reporters_time)
+        server_time = int(datetime.datetime.now().strftime("%s")) * 1000
+
+        offsetTime = long(server_time) - long(reporters_time)
+
     
+        if not partial_distance:
+            partial_distance = 0
+
         if not reporter_id or not pothole_event_data:
             print 'Invalid post data'
             return Response({"success": False, "error": "Invalid POST data"}, status=status.HTTP_400_BAD_REQUEST)
@@ -174,79 +173,97 @@ class AddPothole(APIView):
         values = pothole_event_data.split(':-')
 
         print "Reporter = ",reporter
+
         if(values[0] == 'RideDetails'):
-	
             line = values[1]
-            print line
-            	
             values = line[1:len(line)-1]
             keys_values = values.split(';')
             
             start_time = ""
-            stop_time = ""                                               
-            speed_sum = 0
+            stop_time = ""
+            speed = 0
             version = ""
-	    distance = 0
+            distance = 0
+            phone_model = ""
            
             for i in range(len(keys_values)):
                 key_value = keys_values[i].split(":")
                 key = key_value[0]
-                value = key_value[1]    
-                if(key == "StartTime"):                
-                    start_time = datetime.datetime.fromtimestamp((long(value)+offset)/1000.0)
+                value = key_value[1]
+                if(key == "StartTime"):
+                    start_time = datetime.datetime.fromtimestamp((long(value)+offsetTime)/1000.0)
                     
-                if(key == "StopTime"):                
-                    stop_time = datetime.datetime.fromtimestamp((long(value)+offset)/1000.0)
+                if(key == "StopTime"):
+                    stop_time = datetime.datetime.fromtimestamp((long(value)+offsetTime)/1000.0)
 
-   		if(key == "Distance"):
-	     	    distance =  float(value)             
+                if(key == "Distance"):
+                    distance =  float(value)
                    
-                if(key == "Speed"):
-                    speed_sum = float(value)
+                if(key == "AvgRideSpeed"):
+                    speed = float(value)
                     
                 if(key == "Version"):
-                    version = value                            
+                    version = value
 
-		
-            print 'inserting ride details'
-            RideDetails(reporter=reporter, vehicle_type=vehicle_type, start_time=start_time, stop_time=stop_time, speed=speed_sum,distance = distance, app_version=version, pothole_count = 0 ).save()
+                if(key == "PhoneModel"):
+                    phone_model = value
 
-        else:            
-            data = getData(values[1], int(win_size))    
-            clf = joblib.load('/home/brserver1/safestreet/serverPothole/auto_pothole/model/model.pkl')
+            print 'inserting RideDetails'
+            RideDetails(reporter=reporter, vehicle_type=vehicle_type, start_time=start_time, stop_time=stop_time, speed=speed,distance = distance, app_version=version, pothole_count = 0, phone_model = phone_model ).save()
+        else:
+            parsedData = getParsedData(values[1], int(win_size))
+            smoothData = getSmoothData(parsedData,int(win_size))
+            featuresData = cal_features(smoothData,int(win_size))
+
+            file_path = os.path.join(settings.STATIC_ROOT, 'model/model.pkl')
+            clf = joblib.load(file_path)
             flag = False
             
-            features = map(lambda p:[p[0],p[1]], data)
-            
-            lat = data[0][2]
-            lon = data[0][3]
-            time = data[0][4]
-            probs = []	
+            features = map(lambda p:[p[0],p[1]], featuresData)
+            intensities = []
+
             for i in range(len(features)):
-                predicted = clf.predict([features[i]])
-                p = clf.predict_proba(features[i])
-                probs.append(p.tolist()[0][1]) 
+                feature_vector = features[i]
+                feature_vector = np.array(feature_vector).reshape((1, -1))
+                predicted = clf.predict(feature_vector)
+
+                y = clf.decision_function(feature_vector)
+                w_norm = np.linalg.norm(clf.coef_)
+                dist = y / w_norm
+                dist = dist.tolist()
+                intensities.append(float(dist[0]))
+
                 if(int(predicted[0]) == 1):
                     flag = True
-                    lat = data[i][2]
-                    lon = data[i][3]
-                    time = data[i][4]
+
+            indexOfMaxIntensity = 0
+            maxIntensity = -1
+            for i in range(len(intensities)):
+                if(maxIntensity < intensities[i]):
+                    maxIntensity = intensities[i]
+                    indexOfMaxIntensity = i
+
+            lat = featuresData[indexOfMaxIntensity][2]
+            lon = featuresData[indexOfMaxIntensity][3]
+            time = featuresData[indexOfMaxIntensity][4]
             
 
-            pred_confidence = max(probs) 		                                            
+            print 'indexOfMaxIntensity = ',indexOfMaxIntensity
             if not vehicle_type:
                 vehicle_type = 'D'        
                 
-            dt = datetime.datetime.fromtimestamp((long(time) + offset)/1000.0)   
+            dt = datetime.datetime.fromtimestamp((long(time) + offsetTime)/1000.0)
 
             if(flag == False):
                 print "inserting nonpothole info"
-                AutomatedPotholes(reporter=reporter, event_data=pothole_event_data, vehicle_type=vehicle_type, win_size=win_size, classifier_output='0', detection_time=dt, latitude=lat, longitude=lon, pred_prob = pred_confidence ).save()
-            else:                
+                AutomatedPotholes(reporter=reporter, event_data=pothole_event_data, vehicle_type=vehicle_type, win_size=win_size, classifier_output='0', detection_time=dt, latitude=lat, longitude=lon, intensity_score = maxIntensity, partial_distance = partial_distance ).save()
+            else:
                 print "inserting pothole info"
-                AutomatedPotholes(reporter=reporter, event_data=pothole_event_data, vehicle_type=vehicle_type, win_size=win_size, classifier_output='1', detection_time=dt,latitude=lat, longitude=lon, pred_prob = pred_confidence ).save()
+                AutomatedPotholes(reporter=reporter, event_data=pothole_event_data, vehicle_type=vehicle_type, win_size=win_size, classifier_output='1', detection_time=dt,latitude=lat, longitude=lon, intensity_score = maxIntensity, partial_distance = partial_distance ).save()
                 
+
         return Response({"success": True}, status=status.HTTP_201_CREATED)
+
                      
 class AllRideDetail(APIView):
     """
@@ -255,18 +272,17 @@ class AllRideDetail(APIView):
     def get(self, request, pk):
         ride_details = RideDetails.objects.filter(reporter = pk)
         serializer = RideDetailsEntrySerializer(ride_details, many=True)
-	data = serializer.data
-	total_distance = 0
-	total_time = 0
-	pothole_count = 0
+        data = serializer.data
+        total_distance = 0
+        total_time = 0
+        pothole_count = 0
         
-	for i in range(len(data)):
-		total_distance = total_distance + data[i]["distance"]	
-		d1 = datetime.datetime.strptime(data[i]["start_time"][0:19], '%Y-%m-%dT%H:%M:%S')
-		d2 = datetime.datetime.strptime(data[i]["stop_time"][0:19], '%Y-%m-%dT%H:%M:%S')
-		total_time = total_time + (d2-d1).total_seconds()
-		pothole_count = pothole_count + data[i]["pothole_count"]
-	total_time = total_time*1000
+        for i in range(len(data)):
+            total_distance = total_distance + data[i]["distance"]
+            d1 = datetime.datetime.strptime(data[i]["start_time"][0:19], '%Y-%m-%dT%H:%M:%S')
+            d2 = datetime.datetime.strptime(data[i]["stop_time"][0:19], '%Y-%m-%dT%H:%M:%S')
+            total_time = total_time + (d2-d1).total_seconds()
+            pothole_count = pothole_count + data[i]["pothole_count"]
 
-        return Response({"id":pk,"total_time":long(total_time),"total_distance":total_distance,"pothole_count":pothole_count},status=status.HTTP_200_OK)   
-    
+        total_time = total_time*1000
+        return Response({"id":pk,"total_time":long(total_time),"total_distance":total_distance,"pothole_count":pothole_count},status=status.HTTP_200_OK)
