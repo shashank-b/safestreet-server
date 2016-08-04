43b96b9eb8c7ecf1071470cf920dcae0562ada0d
vikrant <vikrant@cse.iitb.ac.in>
2016-08-04 10:58:56 +0530

    root url is set

--- serverPothole/urls.py
+++ serverPothole/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'),
         name='api-auth'),
+    url(r'^$', include('web.urls', namespace='web')),
     url(r'^web/', include('web.urls', namespace='web')),
     url(r'^auto_pothole/', include('auto_pothole.urls', namespace='auto_pothole'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
