e3a5db8ba68b0cf2c292eecc52adb5f92987a1cb
vikrant <vikrant@cse.iitb.ac.in>
2016-08-08 01:56:32 +0530

    restructure the docs directory

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # SafeStreet documentation build configuration file, created by
-# sphinx-quickstart on Mon Aug  8 01:05:45 2016.
+# sphinx-quickstart on Mon Aug  8 01:55:20 2016.
 #
 # This file is execfile()d with the current directory set to its
 # containing dir.
@@ -82,7 +82,7 @@ language = None
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This patterns also effect to html_static_path and html_extra_path
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 .. SafeStreet documentation master file, created by
-   sphinx-quickstart on Mon Aug  8 01:05:45 2016.
+   sphinx-quickstart on Mon Aug  8 01:55:20 2016.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
