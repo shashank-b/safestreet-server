fa8c7cf1ae75e644d51f3a5b4770f2ee9826475c
vikrant <vikrant@cse.iitb.ac.in>
2016-08-08 10:33:07 +0530

    don't change html_theme for rtd builds do it locally, updated progress.md

--- docs/conf.py
+++ docs/conf.py
@@ -121,11 +121,23 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
+
+import os
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+# otherwise, readthedocs.org uses their theme by default, so no need to specify it
+
+#  import sphinx_rtd_theme
 
 
 #  html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+#  html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -135,7 +147,7 @@ html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom themes here, relative to this directory.
 # html_theme_path = []
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+#  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
--- progress.md
+++ progress.md
@@ -1,5 +1,6 @@
 ## 08-Aug-2016
 1. uploaded the documentation to readthedocs.org [link](http://safestreet-server.readthedocs.io/en/latest/)
+
 ## 06-Aug-2016
 1. put /etc directory to version control which saves the system configuration on daily basis: this is considered as best industry practice
 2. set safestreet to use nginx + gunicorn webserver for local, brserver1 and testbed machines.
