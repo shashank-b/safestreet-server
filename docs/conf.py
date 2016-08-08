01ee9a9494a9350c1beaacdcc01d7f868434a7d5
vikrant <vikrant@cse.iitb.ac.in>
2016-08-08 10:56:18 +0530

    different themes for readthedocs and local

--- docs/conf.py
+++ docs/conf.py
@@ -122,7 +122,12 @@ todo_include_todos = False
 # a list of builtin themes.
 #
 #  html_theme = 'alabaster'
-
+import os
+on_rtd = os.environ.get('READTHEDOCS') == 'True'
+if on_rtd:
+    html_theme = 'default'
+else:
+    html_theme = "sphinx_rtd_theme"
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
