5144459ffeaa90a7dbd86982abbab6fc9aeeb65b
vikrant <vikrant@cse.iitb.ac.in>
2017-03-21 02:17:28 +0530

    negative intensity is also considered

--- scripts/reorient.py
+++ scripts/reorient.py
@@ -1,3 +1,4 @@
+import bisect
 import math
 import zipfile
 from collections import deque
@@ -32,8 +33,8 @@ def get_pothole_timestamps(gps_log):
                 parts = line.split(",")
                 # print(parts)
                 # if len(parts) >= 7 and float(parts[4]) >3:
-                if len(parts) >= 7:
-                    # if len(parts) >= 7 and parts[6] == "y":
+                # if len(parts) >= 7:
+                if len(parts) >= 7 and parts[6] == "y":
                     try:
                         timestamp = int(parts[0])
                         lat = float(parts[1])
@@ -47,6 +48,7 @@ def get_pothole_timestamps(gps_log):
             f.close()
     zip_file.close()
     gps_log.close()
+    pothole_timestamp_list.sort()
     return pothole_timestamp_list
 
 
@@ -184,6 +186,7 @@ def print_deque(eq, pothole_report_time):
 
 
 def smooth(smq):
+    # print("calculating smooth acc")
     sum_ax = 0
     sum_ay = 0
     sum_az = 0
@@ -278,6 +281,108 @@ def reoriented_az(acc_rows, gps_rows):
             cnt += 1
 
 
+def reor_acc(acc, mean_ax, mean_ay, mean_az):
+    deno = mean_az * mean_az + mean_ax * mean_ax
+    if deno != 0:
+        # print("reorienting {}".format(acc))
+        theta_y = math.asin(mean_ax / math.sqrt(deno))
+
+        # Rotation across X axis
+        theta_x = math.atan2(math.sqrt(mean_ax * mean_ax + mean_az * mean_az), mean_ay)
+
+        # Tweak used to handle data for different coordinate axes
+        if mean_az > 0:
+            theta_y = -theta_y
+            theta_x = -theta_x
+
+        cos_y = math.cos(theta_y)
+        sin_y = math.sin(theta_y)
+        cos_x = math.cos(theta_x)
+        sin_x = math.sin(theta_x)
+
+        x = acc.ax
+        y = acc.ay
+        z = acc.az
+
+        tmp = -x * sin_y + z * cos_y
+        new_ax = x * cos_y + z * sin_y
+        new_az = y * cos_x - tmp * sin_x
+        new_ay = y * sin_x + tmp * cos_x
+    else:
+        return A(acc)
+    return A((acc.time, new_ax, new_ay, new_az))
+
+
+def get_intensity(acc_rows, g, trip):
+    # event queue of 1 second
+    eq = deque()
+    # smooth reoriented az queue of .5 second
+    smq = deque()
+    cnt = 0
+    sum_ax = 0
+    sum_ay = 0
+    sum_az = 0
+    # print("finding event_index")
+    event_index = bisect.bisect(acc_rows, (g.time,))
+    start_index = event_index - 1
+    if event_index >= len(acc_rows) or abs(acc_rows[event_index][0] - g.time) >= 1000:
+        print("event_index is not close to acc time")
+        print("event_index = {} len(acc_rows) = {}".format(event_index, len(acc_rows)))
+        if event_index < len(acc_rows):
+            print("a.time = {} g.time = {} |at-et| = {}".format(acc_rows[event_index][0], g.time,
+                                                                abs(acc_rows[event_index][0] - g.time)))
+            print("filename acclogs = {} gpslogs = {}".format(trip.acc_log.name,trip.gps_log.name))
+        return g
+
+    # print("going 15 second back")
+    while start_index > 0 and (acc_rows[event_index][0] - acc_rows[start_index][0]) <= 15000:
+        start_index -= 1
+
+    # print("starting from 15 seconds back and going tille 13.5")
+    while start_index < len(acc_rows) and acc_rows[start_index][0] <= (acc_rows[event_index][0] - 1500):
+        a = A(acc_rows[start_index])
+        if a.is_close_to_9_8():
+            sum_ax += a.ax
+            sum_ay += a.ay
+            sum_az += a.az
+            cnt += 1
+        start_index += 1
+    if cnt != 0:
+        mean_ax = sum_ax / cnt
+        mean_ay = sum_ay / cnt
+        mean_az = sum_az / cnt
+        # print("calculating mean for reor")
+    else:
+        print("no point close to 9.8")
+        return g
+    # print("adding 500 ms worth of smooth data to queue")
+    while start_index < len(acc_rows) and acc_rows[start_index][0] <= (acc_rows[event_index][0] - 1000):
+        a = A(acc_rows[start_index])
+        smq.append(reor_acc(a, mean_ax, mean_ay, mean_az))
+        start_index += 1
+
+    # et -1 to et + 1 select maximum sd to gps location
+    # print("calulating max sd in event window")
+    while start_index < len(acc_rows) and acc_rows[start_index][0] < (acc_rows[event_index][0] + 1000):
+        racc = reor_acc(A(acc_rows[start_index]), mean_ax, mean_ay, mean_az)
+        smq.append(racc)
+        sacc = smooth(smq)
+        # print("racc = {}".format(sacc.az))
+        eq.append(sacc)
+        if (eq[-1].time - eq[0].time) >= 900:
+            sd, max_min, mean = sd_and_max_min(eq)
+            if sd > g.sd:
+                # print("setting sd to {}".format(sd))
+                g.sd = sd
+                g.max_min = max_min
+        while (eq[-1].time - eq[0].time) >= 1000:
+            eq.popleft()
+        while smq[-1].time - smq[0].time >= 500:
+            smq.popleft()
+        start_index += 1
+    return g
+
+
 clf = None
 
 
@@ -318,15 +423,18 @@ def print_gps_row(gps_rows, trip):
         #     cnt +=1
         #     gps_file_name = gps_file.name
         #     acc_file_name = acc_file.name
-        if g.sd > 0 and g.sd * g.sd >= .2:
+        if g.sd > 0:
             feature_vector = [g.sd, g.max_min]
             pred = clf.predict([feature_vector])[0]
-            if pred.strip() == '1':
-                dist = clf.decision_function([feature_vector])
-                g.intensity = dist[0]
-                g.trip_id = trip.id
-                if g.intensity >= 1.0:
-                    print(g, file=fw)
+            # if pred.strip() == '1':
+            dist = clf.decision_function([feature_vector])
+            g.intensity = dist[0]
+            g.trip_id = trip.id
+            print(g, file=fw)
+            # if g.intensity >= 1.0:
+            #     print(g, file=fw)
+            # else:
+            #     print(g)
 
 
 def print_gps_row2(gps_rows):
@@ -334,65 +442,61 @@ def print_gps_row2(gps_rows):
         print("{:.6f},{:.6f}".format(g[1], g[2]), file=fw)
 
 
+def get_acc_rows(acc_log):
+    acc_rows = []
+    try:
+        zip_file = zipfile.ZipFile(acc_log)
+        file_names = zip_file.namelist()
+    except zipfile.BadZipFile as ex:
+        print("bad zip file {0}".format(acc_log.name))
+        return acc_rows
+    for file_name in file_names:
+        # print(file_name)
+        # print(cnt)
+        with zip_file.open(file_name) as f:
+            # pass
+            first = True
+            for line in f:
+                line = line.decode("utf-8").strip()
+                if first:  # skip the first header row
+                    header = line.split(",")
+                    if len(header) != 4:
+                        break
+                    first = False
+                    continue
+                parts = line.split(",")
+                if len(parts) == 4:
+                    try:
+                        time = int(parts[0])
+                        ax = float(parts[1])
+                        ay = float(parts[2])
+                        az = float(parts[3])
+                    except Exception as ex:
+                        print(ex)
+                        continue
+                    # print(time, ax, ay, az)
+                    acc_rows.append((time, ax, ay, az))
+            # print(pothole_timestamps)
+            # print(timestamps)
+            # print("========")
+            f.close()
+    zip_file.close()
+    acc_rows.sort()
+    return acc_rows
+
+
 def reorient():
     load_model()
     trips = Ride.objects.all()
     for trip in trips:
-        acc_log = trip.acc_log
         gps_rows = get_pothole_timestamps(trip.gps_log)
-        # print_gps_row2(gps_rows)
-        # print(len(gps_rows))
-        # continue
         if len(gps_rows) == 0:
             continue
-        try:
-            zip_file = zipfile.ZipFile(acc_log)
-            file_names = zip_file.namelist()
-        except zipfile.BadZipFile as ex:
-            print("bad zip file {0}".format(acc_log.name))
+        acc_rows = get_acc_rows(trip.acc_log)
+        if len(acc_rows) == 0:
             continue
-        try:
-            for file_name in file_names:
-                acc_rows = []
-                # print(file_name)
-                # print(cnt)
-                with zip_file.open(file_name) as f:
-                    # pass
-                    first = True
-                    for line in f:
-                        line = line.decode("utf-8").strip()
-                        if first:  # skip the first header row
-                            header = line.split(",")
-                            if len(header) != 4:
-                                break
-                            first = False
-                            continue
-                        parts = line.split(",")
-                        if len(parts) == 4:
-                            try:
-                                time = int(parts[0])
-                                ax = float(parts[1])
-                                ay = float(parts[2])
-                                az = float(parts[3])
-                            except Exception as ex:
-                                print(ex)
-                                continue
-                            # print(time, ax, ay, az)
-                            acc_rows.append((time, ax, ay, az))
-                    # print(pothole_timestamps)
-                    # print(timestamps)
-                    # print("========")
-                    f.close()
-            acc_rows.sort()
-            gps_rows.sort()
-            if len(gps_rows) > 10:
-                for i in range(len(gps_rows)):
-                    gps_rows[i] = G(gps_rows[i])
-                get_pothole_info(acc_rows, gps_rows)
-                # print_pothole_reports(gps_rows)
-                print_gps_row(gps_rows, trip)
-            acc_log.close()
-            zip_file.close()
-        except Exception as ex:
-            print(trip.acc_log.name, trip.gps_log.name)
-            print(ex)
+        gps_rows.sort()
+        for i in range(len(gps_rows)):
+            gps_rows[i] = G(gps_rows[i])
+            get_intensity(acc_rows, gps_rows[i], trip)
+        print_gps_row(gps_rows, trip)
